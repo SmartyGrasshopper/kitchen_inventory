a923e9355165c00367d7dea1b7b4db77c277c282
SmartyGrasshopper <parrotabhijeet@gmail.com>
2025-04-21 04:03:37 +0530

    Added the backend for accounts page. As per currently expected features, the page is fully functioning.

--- kinventory/inventory.py
+++ kinventory/inventory.py
@@ -4,8 +4,10 @@ from flask import (
     flash, redirect, url_for, g
 )
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from kinventory.database import get_db
-from kinventory.auth import signin_required
+from kinventory.auth import signin_required, load_logged_in_user
 
 bp = Blueprint('inventory', __name__, url_prefix='/inventory')
 
@@ -34,7 +36,45 @@ def menu():
 def analytics():
     return render_template("inventory_views/analytics.html")
 
-@bp.route("/account", methods=('GET',))
+@bp.route("/account", methods=('GET', 'POST'))
 @signin_required
 def account():
+    if(request.method == 'POST'):
+        if('change_business_name' in request.form):
+            # form to change business name is received
+
+            db = get_db()
+            db.execute(
+                "UPDATE users SET business_name = ? WHERE username = ?;",
+                (request.form['business_name'], g.user['username'])
+            )
+            db.commit()
+
+            flash("Business Name updated successfully.")
+
+            load_logged_in_user()  #updating the details stored in g-object before proceeding
+            
+
+        elif('change_password' in request.form):
+            # form to change password is received
+            
+            if(check_password_hash(g.user['psword'], request.form['current_password'])):
+                db = get_db()
+                db.execute(
+                    "UPDATE users SET psword = ? WHERE username = ?;",
+                    (generate_password_hash(request.form['new_password']), g.user['username'])
+                )
+                db.commit()
+
+                session.clear()   # signing/logging out
+
+                flash("Password updated successfully. Please sign-in again.")
+
+                return redirect(url_for('auth.signin'))
+
+            else:
+                flash("Failed to change password. Invalid current-password entered.")
+        else:
+            flash("Error. No action available for POSTed form at this route.")
+
     return render_template("inventory_views/account.html")
