b90e841dd90a5cd9924c5f16bdf53ad89ef30c56
SmartyGrasshopper <parrotabhijeet@gmail.com>
2025-04-20 21:42:17 +0530

    Added skeleton code and files (html templates, css files and routes) for all the pages for inventory of a user.

--- kinventory/inventory.py
+++ kinventory/inventory.py
@@ -9,6 +9,31 @@ from kinventory.auth import signin_required
 
 bp = Blueprint('inventory', __name__, url_prefix='/inventory')
 
+@bp.route("/inventory", methods=('GET',))
+@signin_required
+def inventory():
+    return render_template("inventory_views/inventory.html")
+
+@bp.route("/supply", methods=('GET',))
+@signin_required
+def supply():
+    return render_template("inventory_views/supply.html")
+
+@bp.route("/consumption", methods=('GET',))
+@signin_required
+def consumption():
+    return render_template("inventory_views/consumption.html")
+
+@bp.route("/menu", methods=('GET',))
+@signin_required
+def menu():
+    return render_template("inventory_views/menu.html")
+
+@bp.route("/analytics", methods=('GET',))
+@signin_required
+def analytics():
+    return render_template("inventory_views/analytics.html")
+
 @bp.route("/account", methods=('GET',))
 @signin_required
 def account():
